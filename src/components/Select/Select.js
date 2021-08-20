b68d6fa4e66a75a5f03ef5f0e92394e84261421e
colefiscus <coletfiscus@gmail.com>
2021-08-20 15:41:48 -0600

    Redo Select from scratch

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -10,31 +10,60 @@ const Select = ({ label, value, onChange, children }) => {
 
   return (
     <Wrapper>
-      <SelectElement value={value} onChange={onChange}>
+      <NativeSelect value={value} onChange={onChange}>
         {children}
-      </SelectElement>
-      <Chevron id="chevron-down"></Chevron>
+      </NativeSelect>
+      <FancyWrapper>
+        {displayedValue}
+        <IconWrapper>
+          <Icon id="chevron-down" strokeWidth={1} size={24}></Icon>
+        </IconWrapper>
+      </FancyWrapper>
     </Wrapper>
   );
 };
 
-const Wrapper = styled.div`
-  position: relative;
-  width: fit-content;
-`;
-
-const SelectElement = styled.select`
-  background-color: ${COLORS.transparentGray15};
-  color: ${COLORS.gray700};
-  padding: 16px 12px;
-  border-radius: 8px;
-  width: fit-content;
-`;
-
-const Chevron = styled(Icon)`
+const IconWrapper = styled.div`
   position: absolute;
-  right: 0px;
-  top: 15px;
+  top: 0;
+  bottom: 0;
+  right: 6px;
+  margin: auto;
+  width: 24px;
+  height: 24px;
+  pointer-events: none;
+`;
+
+const NativeSelect = styled.select`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+`;
+
+const FancyWrapper = styled.div`
+  color: ${COLORS.gray700};
+  font-size: (16 / 16)rem;
+  background-color: ${COLORS.transparentGray15};
+  padding: 12px 16px;
+  padding-right: 52px;
+  border-radius: 8px;
+
+  ${NativeSelect}:focus + & {
+    outline: 1px dotted #212121;
+    outline: 5px auto -webkit-focus-ring-color;
+  }
+
+  ${NativeSelect}:hover + & {
+    color: ${COLORS.black}
+  }
+  `;
+  
+  const Wrapper = styled.div`
+  width: max-content;
+  position: relative;
 `;
 
 export default Select;
