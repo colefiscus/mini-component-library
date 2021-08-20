9bc350e05fa9783419e1635aa6c12355cce361c0
colefiscus <coletfiscus@gmail.com>
2021-08-19 18:46:23 -0600

    make changes

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -6,11 +6,32 @@ import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
 const ProgressBar = ({ value, size }) => {
+
+  const style = {
+    small: {
+      padding: 0,
+      height: 8,
+      radius: 4
+    },
+    medium: {
+      padding: 0,
+      height: 12,
+      radius: 4
+    },
+    large: {
+      padding: 4,
+      height: 24,
+      radius: 8
+    }
+  }
+
   return <Container>
-          <BarWrapper
-            value={value}
-            max={100}>
+          <BarWrapper>
+            <Bar style={
+              'width'=(370 / value) + '%'
+            }>
               {value}
+            </Bar>
           </BarWrapper>
         </Container>
 };
@@ -19,14 +40,17 @@ const Container = styled.div`
   width: 370px;
   background-color: ${COLORS.transparentGray15};
   box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  border-radius: 8px;
+  padding: 4px;
 `;
 
 const BarWrapper = styled.div`
   width: 370px;
-`;
-
-const Bar = styled.div`
+  `;
   
+const Bar = styled.div`
+  background-color: ${COLORS.primary};
+  border-radius: 8px;
 `;
 
 export default ProgressBar;
