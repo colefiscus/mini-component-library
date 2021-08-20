22387dee5c8b84cb74a5228df2d1639af6dc0607
colefiscus <coletfiscus@gmail.com>
2021-08-20 17:05:17 -0600

    Start on IconSelect

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -6,6 +6,11 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import VisuallyHidden from '../VisuallyHidden';
 
+const STYLES = {
+  large: 300,
+  small: 150,
+}
+
 const IconInput = ({
   label,
   icon,
@@ -13,7 +18,47 @@ const IconInput = ({
   size,
   placeholder,
 }) => {
-  return 'TODO';
+  const inputSize = STYLES[size];
+  const inputIcon = Icon[icon];
+
+  return <Wrapper>
+          <IconWrapper>
+            <Icon id={icon} strokeWidth={1} size={16}></Icon>
+          </IconWrapper>
+          <Input placeholder={placeholder} style={{"--width": inputSize + "px"}} />     
+         </Wrapper>;
 };
+
+const Input = styled.input`
+  padding-left: 22px;
+  border: none;
+  color: ${COLORS.gray700};
+  font-weight: 700;
+  font-size: ${14 / 16}rem;
+  width: var(--width);
+  
+  &::placeholder {
+    color: ${COLORS.gray500};
+    font-weight: 400;
+    font-size: ${14 / 16}rem;
+  }
+`;
+
+const IconWrapper = styled.div`
+  position: absolute;
+  left: 0;
+  top: 0;
+  bottom: 0;
+  margin: auto;
+  width: 16px;
+  height: 16px;
+  pointer-events: none;
+`;
+
+const Wrapper = styled.div`
+  width: fit-content;
+  position: relative;
+  border-bottom: 1px solid black;
+`;
 
 export default IconInput;
