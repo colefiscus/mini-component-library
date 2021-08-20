66916bf14558fd4f3b86127347a0221d99b041ab
colefiscus <coletfiscus@gmail.com>
2021-08-19 18:27:21 -0600

    Start progressbar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -6,7 +6,23 @@ import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  return <Container>
+          <ProgressElement
+            value={value}
+            max={100}>
+              {value}
+          </ProgressElement>
+        </Container>
 };
+
+const Container = styled.div`
+  width: 370px;
+  background-color: ${COLORS.transparentGray15};
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+`;
+
+const ProgressElement = styled.progress`
+  width: 370px;
+`;
 
 export default ProgressBar;
