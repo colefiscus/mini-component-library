e71f500fe4b184b8cc94268b7854c78cd094f9e7
colefiscus <coletfiscus@gmail.com>
2021-08-22 10:48:14 -0600

    Redo IconInput from scratch

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -6,59 +6,48 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import VisuallyHidden from '../VisuallyHidden';
 
-const STYLES = {
-  large: 300,
-  small: 150,
-}
-
-const IconInput = ({
-  label,
-  icon,
-  width = 250,
-  size,
-  placeholder,
-}) => {
-  const inputSize = STYLES[size];
-  const inputIcon = Icon[icon];
+const IconInput = ({label,icon,width = 250,size,placeholder}) => {
 
   return <Wrapper>
+          <VisuallyHidden>{label}</VisuallyHidden>
           <IconWrapper>
-            <Icon id={icon} strokeWidth={1} size={16}></Icon>
+            <Icon id="search" strokeWidth={1} size={16} />
           </IconWrapper>
-          <Input placeholder={placeholder} style={{"--width": inputSize + "px"}} />     
-         </Wrapper>;
+          <TextInput placeholder={placeholder} />
+        </Wrapper>;
 };
 
-const Input = styled.input`
-  padding-left: 22px;
-  border: none;
-  color: ${COLORS.gray700};
-  font-weight: 700;
-  font-size: ${14 / 16}rem;
-  width: var(--width);
-  
-  &::placeholder {
-    color: ${COLORS.gray500};
-    font-weight: 400;
-    font-size: ${14 / 16}rem;
+const Wrapper = styled.label`
+  position: relative;
+  color: ${COLORS.gray500};
+
+  &:hover {
+    color: ${COLORS.black};
   }
 `;
 
 const IconWrapper = styled.div`
   position: absolute;
-  left: 0;
   top: 0;
   bottom: 0;
-  margin: auto;
-  width: 16px;
   height: 16px;
-  pointer-events: none;
+  width: 16px;
+  margin: auto;
 `;
 
-const Wrapper = styled.div`
-  width: fit-content;
-  position: relative;
-  border-bottom: 1px solid black;
+const TextInput = styled.input`
+  padding-left: 24px;
+  height: ${24 / 16}rem;
+  border: none;
+  border-bottom: 1px solid ${COLORS.black};
+  color: inherit;
+  font-weight: 700;
+  outline-offset: 2px;
+
+  &::placeholder {
+    color: ${COLORS.gray500};
+    font-weight: 400;
+  }
 `;
 
 export default IconInput;
